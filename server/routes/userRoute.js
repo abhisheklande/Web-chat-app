4be77394426c8a51aa621678ad9c13c0cd43d2af
Abhi <abhisheklande028@gmail.com>
2022-09-12 20:53:26 +0530

    Bug Fix

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,4 +1,4 @@
-const {register, login, setAvatar, getAllUsers} = require("../controllers/userController")
+const {register, login, setAvatar, getAllUsers, logOut} = require("../controllers/userController")
 
 const router = require('express').Router();
 
@@ -6,6 +6,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/setAvatar/:id", setAvatar);
 router.get("/allusers/:id", getAllUsers);
-
+router.get("/logout/:id", logOut);
 
 module.exports = router;
