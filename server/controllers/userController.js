596cebecbff234a69c5369ebff2e8cd42ddb5fb2
Abhi <abhisheklande028@gmail.com>
2022-09-12 20:51:11 +0530

    Bug fixing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -77,4 +77,13 @@ module.exports.getAllUsers = async (req, res, next) => {
         next(ex);
     }
 };
-  
+
+module.exports.logOut = (req, res, next) => {
+    try {
+      if (!req.params.id) return res.json({ msg: "User id is required " });
+      onlineUsers.delete(req.params.id);
+      return res.status(200).send();
+    } catch (ex) {
+      next(ex);
+    }
+  };
